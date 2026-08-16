0a2d2a7735e455de82de2689298a034ec5c75c1d
Kawa Vinicius <kawavinicioss@gmail.com>
2026-08-16 00:33:41 -0300

    feat: adicionar exercicio de polimorfismo.

---
+++ 15-Polimorfismo/src/Ave.java
@@ -0,0 +1,28 @@
+public class Ave extends Animal {
+    private String corPena;
+
+    public void fazerNinho(){
+        System.out.println("Fazendo o ninho! ");
+    }
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Ave cantando! ");
+    }
+    @Override
+    public void locomover() {
+        System.out.println("Voando! ");
+    }
+    @Override
+    public void alimentar() {
+        System.out.println("Bicando! ");
+    }
+}
